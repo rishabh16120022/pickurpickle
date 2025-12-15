ebb93e5c2eedaf32622a8726935bc7f8ca9fc324
rishabh16120022 <rishutripathi161@gmail.com>
2025-12-15 14:55:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -143,6 +143,6 @@ app.post('/api/config', async (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
